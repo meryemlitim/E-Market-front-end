5fcab7ed379a6b7df165d3d6230c3ed7892cafce
meryemlitim <litimmeryem2001@gmail.com>
2025-11-04 16:51:15 +0100

    feat: implement authentication forms for login and registration

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
 import Register from "./pages/register.jsx";
+import Login from "./pages/login.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
 
 
 
--- front-end/src/pages/register.jsx
+++ front-end/src/pages/register.jsx
@@ -1,57 +1,8 @@
+import AuthForm from "../components/AuthForm";
+
 export default function Register() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md border border-gray-200">
-        <h1 className="text-2xl font-bold text-center text-violet-600 mb-6">
-          Register Now
-        </h1>
+    <AuthForm type="register" />
 
-        <form className="space-y-4">
-          {/* Full Name */}
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              Full Name
-            </label>
-            <input
-              type="text"
-              placeholder="Enter your full name"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
-            />
-          </div>
-
-          {/* Email */}
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              Email
-            </label>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
-            />
-          </div>
-
-          {/* Password */}
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              Password
-            </label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
-            />
-          </div>
-
-          {/* Button */}
-          <button
-            type="submit"
-            className="w-full bg-violet-600 text-white py-2 rounded-lg font-medium hover:bg-violet-700 transition-all"
-          >
-            Register
-          </button>
-        </form>
-      </div>
-    </div>
   );
 }
